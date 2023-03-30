800713c913849d4cedda273d021530b648f1e56f
Raúl <raulbaeza8@gmail.com>
2023-03-30 11:40:43 +0200

    sql cuenta filas

---
+++ Mis Consultas/Numero de elementos en cada tabla.sql
@@ -0,0 +1,7 @@
+select count(*) as categoriaproducto from categoriaproducto;
+select count(*) as clientes from clientes;
+select count(*) as detallepedidos from detallepedidos;
+select count(*) as pedidos from pedidos;
+select count(*) as productos from productos;
+select count(*) as subcategoriaproducto from subcategoriaproducto;
+select count(*) as territorioventas from territorioventas;
