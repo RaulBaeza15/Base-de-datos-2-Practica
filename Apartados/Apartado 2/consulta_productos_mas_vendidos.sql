473410d5c442a2e16c13b8c63322bccf3f51d7eb
Raúl <raulbaeza8@gmail.com>
2023-03-30 12:05:42 +0200

    primera consulta con el in

--- Apartados/Apartado 2/consulta_productos_mas_vendidos.sql
+++ Apartados/Apartado 2/consulta_productos_mas_vendidos.sql
@@ -1,1 +1,12 @@
-Select * from pedidos
+Select nombre_producto from productos;
+Select count(nombre_producto) from productos;
+Select count(distinct nombre_producto) from productos;
+Select * from pedidos;
+Select * from detallepedidos;
+Select * from detallepedidos
+where cantidad > 5;
+
+
+select * from productos 
+where ProductoID in (Select * from detallepedidos
+where cantidad > 5);
