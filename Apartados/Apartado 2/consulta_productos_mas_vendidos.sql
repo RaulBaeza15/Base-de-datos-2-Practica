eb4992f44566cdc3fe76ddbef78d7fe3da9af7b5
Raúl <raulbaeza8@gmail.com>
2023-03-30 12:30:57 +0200

    apartado 2 con join, exists, in e inner join

--- Apartados/Apartado 2/consulta_productos_mas_vendidos.sql
+++ Apartados/Apartado 2/consulta_productos_mas_vendidos.sql
@@ -7,6 +7,50 @@ Select * from detallepedidos
 where cantidad > 5;
 
 
-select * from productos 
-where ProductoID in (Select * from detallepedidos
+# Con el in
+select nombre_producto  
+from productos 
+where ProductoID in (Select ProductoID 
+from detallepedidos
 where cantidad > 5);
+
+# Count con el in
+select count(nombre_producto  ) as "In"
+from productos 
+where ProductoID in (Select ProductoID 
+from detallepedidos
+where cantidad > 5);
+
+# con el join
+SELECT distinct nombre_producto 
+FROM productos
+JOIN detallepedidos ON productos.`ProductoID` = detallepedidos.ProductoID
+where detallepedidos.cantidad>5;
+
+# Count con el join
+SELECT count(distinct nombre_producto) as "Join"
+FROM productos
+JOIN detallepedidos ON productos.`ProductoID` = detallepedidos.ProductoID
+where detallepedidos.cantidad>5;
+
+# Con el Exists
+Select nombre_producto from productos
+where exists (select * from detallepedidos where productos.`ProductoID` = detallepedidos.ProductoID 
+and cantidad >5 );
+# Count Con el Exists
+Select count(nombre_producto) as "Exists" from productos
+where exists (select * from detallepedidos where productos.`ProductoID` = detallepedidos.ProductoID 
+and cantidad >5 );
+
+# Con el Inner join 
+SELECT distinct nombre_producto 
+FROM productos
+inner JOIN detallepedidos ON productos.`ProductoID` = detallepedidos.ProductoID
+where detallepedidos.cantidad>5;
+
+# Count Con el Inner join 
+SELECT count(distinct nombre_producto) as "Inner join"
+FROM productos
+inner JOIN detallepedidos ON productos.`ProductoID` = detallepedidos.ProductoID
+where detallepedidos.cantidad>5;
+
