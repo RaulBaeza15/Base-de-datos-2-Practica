9d87a8a0f3d6e00fec72fb60ef1023cd04c85b37
RaulBaeza15 <95641914+RaulBaeza15@users.noreply.github.com>
2023-04-18 18:31:56 +0200

    formateo consultas

--- Apartados/Apartado 2/Exists/consulta_productos_mas_vendidos_Exists.sql
+++
@@ -1,8 +0,0 @@
-# Con el Exists
-Select nombre_producto from productos
-where exists (select * from detallepedidos where productos.`ProductoID` = detallepedidos.ProductoID 
-and cantidad >5 );
-# Count Con el Exists
-Select count(nombre_producto) as "Exists" from productos
-where exists (select * from detallepedidos where productos.`ProductoID` = detallepedidos.ProductoID 
-and cantidad >5 );
