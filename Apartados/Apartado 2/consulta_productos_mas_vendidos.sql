e848624cbefa25c3b6e452de2c91b47e1608f689
RaulBaeza15 <95641914+RaulBaeza15@users.noreply.github.com>
2023-04-18 18:31:39 +0200

    formateo consultas

--- Apartados/Apartado 2/consulta_productos_mas_vendidos.sql
+++ Apartados/Apartado 2/consulta_productos_mas_vendidos.sql
@@ -1,20 +1,22 @@
-# Con el in
-select nombre_producto  
+# Con el In 0.094 sec / 0.000 sec
+SELECT nombre_producto  as 'In'
 from productos 
 where ProductoID in (Select ProductoID 
 from detallepedidos
 where cantidad > 5);
-# con el join
-SELECT distinct nombre_producto 
+
+# Con el Join 0.078 sec / 0.000 sec
+SELECT distinct nombre_producto as 'Join'
 FROM productos
 JOIN detallepedidos ON productos.`ProductoID` = detallepedidos.ProductoID
 where detallepedidos.cantidad>5;
-# Con el Exists
-Select nombre_producto from productos
+
+# Con el Exists 0.094 sec / 0.000 sec
+SELECT nombre_producto as 'Exists' from productos 
 where exists (select * from detallepedidos where productos.`ProductoID` = detallepedidos.ProductoID 
 and cantidad >5 );
 
-# Con el Inner join 
+# Con el Inner join 0.094 sec / 0.000 sec
 SELECT distinct nombre_producto  as 'Inner Join'
 FROM productos
 inner JOIN detallepedidos ON productos.`ProductoID` = detallepedidos.ProductoID
