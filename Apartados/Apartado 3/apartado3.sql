75b3f8232fad800d4899be44ebe943c9ec0c1fde
RaulBaeza15 <95641914+RaulBaeza15@users.noreply.github.com>
2023-04-18 19:55:48 +0200

    consulta apartado 3

--- Apartados/Apartado 3/apartado3.sql
+++ Apartados/Apartado 3/apartado3.sql
@@ -1,56 +1,30 @@
-SELECT p.nombre_producto, COUNT(*) AS num_pedidos
-FROM productos p
-INNER JOIN subcategoriaproducto s
-    ON p.subcategoriaID = s.subCategoriaID
-INNER JOIN categoriaproducto c
-    ON s.categoriaID = c.categoriaID
-INNER JOIN detallepedidos d
-    ON p.ProductoID = d.ProductoID
-WHERE c.nombre_categoria = 'Componente'
-    AND s.nombre_subcategoria LIKE '%Cuadro%'
-    AND p.color = 'Blue'
-GROUP BY p.nombre_producto
-HAVING COUNT(*) > (
-    SELECT COUNT(*)
-    FROM subcategoriaproducto sc
-    INNER JOIN productos pd
-        ON sc.subCategoriaID = pd.subcategoriaID
-    INNER JOIN detallepedidos dp
-        ON pd.ProductoID = dp.ProductoID
-    WHERE sc.nombre_subcategoria IN ('Dirección', 'Horquilla')
-    );
-select * from categoriaproducto cat
-where cat.nombre_categoria='Componente';
-select * from subcategoriaproducto subcat
-where subcat.nombre_subcategoria  LIKE '%Cuadro%';
-select * from productos prod
-where prod.color='blue';
-select * from subcategoriaproducto subcat
-where subcat.nombre_subcategoria = 'Dirección';
-select * from subcategoriaproducto subcat
-where subcat.nombre_subcategoria  = 'Horquilla';
-
-select * from productos prod 
-where prod.subcategoriaID in (select subcat.subcategoriaID from subcategoriaproducto subcat
-where subcat.nombre_subcategoria  = 'Horquilla'); 
-
 select dtllp.ProductoID, count(distinct dtllp.pedidoId) 
 from detallepedidos dtllp
 WHERE dtllp.ProductoID in (
-	select prod.ProductoID from productos prod 
-	where prod.subcategoriaID in (
+	select prod.ProductoID from productos prod
+	where prod.color='blue' 
+    and prod.subcategoriaID in (
 		select subcat.subcategoriaID from subcategoriaproducto subcat
-		where subcat.nombre_subcategoria  = 'Dirección'))
-GROUP BY dtllp.ProductoID;
-            
-select dtllp.ProductoID, count(distinct dtllp.pedidoId) 
-from detallepedidos dtllp
-WHERE dtllp.ProductoID in (
-	select prod.ProductoID from productos prod 
-	where prod.subcategoriaID in (
-		select subcat.subcategoriaID from subcategoriaproducto subcat
-		where subcat.nombre_subcategoria  = 'Horquilla'))
-GROUP BY dtllp.ProductoID;
-
-
- 
+		where subcat.nombre_subcategoria  LIKE '%Cuadro%'
+		and subcat.categoriaID in (
+			select cat.categoriaID from categoriaproducto cat
+			where cat.nombre_categoria='Componente')))
+GROUP BY dtllp.ProductoID
+HAVING count(distinct dtllp.pedidoId)>=all (
+	select count(distinct dtllp.pedidoId) 
+	from detallepedidos dtllp
+	WHERE dtllp.ProductoID in (
+		select prod.ProductoID from productos prod 
+		where prod.subcategoriaID in (
+			select subcat.subcategoriaID from subcategoriaproducto subcat
+			where subcat.nombre_subcategoria  = 'Horquilla'))
+	GROUP BY dtllp.ProductoID) 
+or count(distinct dtllp.pedidoId) >= all(
+	select  count(distinct dtllp.pedidoId) 
+	from detallepedidos dtllp
+	WHERE dtllp.ProductoID in (
+		select prod.ProductoID from productos prod 
+		where prod.subcategoriaID in (
+			select subcat.subcategoriaID from subcategoriaproducto subcat
+			where subcat.nombre_subcategoria  = 'Dirección'))
+	GROUP BY dtllp.ProductoID);
