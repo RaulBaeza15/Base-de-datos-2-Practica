3df146df3d078b0633a980c7eb8bc1f15904a74f
Raúl Baeza <raulbaeza8@gmail.com>
2023-04-20 11:29:25 +0200

    Sql 5

--- Apartados/Apartado 5/Apartado5.sql
+++ Apartados/Apartado 5/Apartado5.sql
@@ -1,1 +1,21 @@
-select * from pedidos
+START TRANSACTION;
+
+UPDATE detallepedidos
+
+Set descuento_unitario=descuento_unitario+1
+
+WHERE PedidoID IN (SELECT PedidoID 
+
+ FROM pedidos INNER JOIN detallespedidos ON pedidos.PedidoID=detallepedidos.PedidoID
+
+INNER JOIN producto ON detallepedidos.productoID=producto.productoID 
+
+INNER JOIN subcategoriaproducto ON productos.subcategoriaID=subcategoriaproducto.subcategoriaID
+
+INNER JOIN categoriaproducto ON categoriaproducto.categoriaID=subcategoriaproducto.categoriaID
+
+  WHERE YEAR(fecha_venta) = 2023
+
+) AND nombre_categoria = 'Bicicleta' AND descuento_unitario > 0;
+
+ROLLBACK;
