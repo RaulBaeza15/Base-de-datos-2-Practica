f791c404fbd6b34ae38756b4cd90d6b0a9a56585
RaulBaeza15 <95641914+RaulBaeza15@users.noreply.github.com>
2023-04-23 18:21:27 +0200

    apartado6

---
+++ Apartados/Apartado 6/CreaccionTabla.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tabla_pedidos (
+    pedidoID INT,
+    primer_nombre VARCHAR(50),
+    apellidos VARCHAR(50),
+    territorioID INT,
+    fecha_venta DATE,
+    total_pedido DECIMAL(10, 2)
+);
--- Apartados/Apartado 6/InsertTabla.sql
+++ Apartados/Apartado 6/InsertTabla.sql
@@ -1,6 +1,6 @@
+INSERT INTO tabla_pedidos (pedidoID, primer_nombre, apellidos, territorioID, fecha_venta, total_pedido)
 SELECT p.pedidoID, c.primer_nombre, c.apellidos, c.territorioID, p.fecha_venta, SUM((d.precio_unitario - d.descuento_unitario) * d.cantidad) AS total_pedido
 FROM pedidos p
 INNER JOIN clientes c ON p.clienteID = c.clienteID
 INNER JOIN detallepedidos d ON p.pedidoID = d.pedidoID
 GROUP BY p.pedidoID;
-
